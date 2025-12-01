bd079bfb7b4b098beba97bf40132013bb3f44540
itsmdevteam <97892749+itsmdevteam@users.noreply.github.com>
2025-12-01 16:03:24 +0100

    Bump version from 2.0.0 to 2.0.1

--- setup.php
+++ setup.php
@@ -61,7 +61,7 @@ function plugin_init_stockmanagement() {
 function plugin_version_stockmanagement() {
 	return array(
 		'name'           => __('Stock management', 'stockmanagement'),
-		'version'        => '2.0.0',
+		'version'        => '2.0.1',
 		'license'        => 'AGPLv3+',
 		'author'         => 'ITSM Dev Team',
 		'homepage'       => 'https://github.com/itsmng/stockmanagement',
